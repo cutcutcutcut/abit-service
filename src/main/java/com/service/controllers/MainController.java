a3f26f61a79a58f564c65aa458e5d9c717c482f7
cutcutcutcut <bekhterev98@mail.ru>
2021-07-17 19:38:38 +0400

    add: javadoc

--- src/main/java/com/service/controllers/MainController.java
+++ src/main/java/com/service/controllers/MainController.java
@@ -56,7 +56,7 @@ public class MainController {
      * @return String
      */
     @GetMapping("/abits")
-    public String testDb(Model model) {
+    public String abitsTable(Model model) {
         model.addAttribute("abits", abitRepo.findAll());
         return "addTable";
     }
